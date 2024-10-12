7069748285e83a0691f87cddf2c8de43c0a08937
Marcelo Vargas <brayanmvy2003@gmail.com>
2024-10-12 10:43:08 -0400

    Bug Corregido

--- Wanca/Assets/Marsel/Scripts/S/Line.cs
+++ Wanca/Assets/Marsel/Scripts/S/Line.cs
@@ -1,11 +1,10 @@
-﻿using System.Numerics;
-using UnityEditor.Experimental.GraphView;
-using UnityEngine;
+﻿using UnityEngine;
 public class Line
 {
     Orientation orientation;
     Vector2Int coordinates;
-    public Line (Orientation orientation,Vector2Int coordinates)
+
+    public Line(Orientation orientation, Vector2Int coordinates)
     {
         this.orientation = orientation;
         this.coordinates = coordinates;
@@ -14,8 +13,9 @@ public class Line
     public Orientation Orientation { get => orientation; set => orientation = value; }
     public Vector2Int Coordinates { get => coordinates; set => coordinates = value; }
 }
+
 public enum Orientation
 {
-    horizontal =0,
-    vertical = 1,
+    Horizontal = 0,
+    Vertical = 1
 }
